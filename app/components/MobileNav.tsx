eb572e57186341bd8dbc66d4a633c8946be033ea
Florent HAJANIRINA <hhf.hajanirina@gmail.com>
2025-12-12 20:24:32 +0100

    fix menu in mobile

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -31,20 +31,26 @@ export default function MobileNav() {
       </button>
 
       {isOpen && (
-        <div className="fixed inset-0 top-16 bg-white z-40 md:hidden">
-          <nav className="flex flex-col p-6 space-y-4">
-            {navItems.map((item) => (
-              <a
-                key={item.href}
-                href={item.href}
-                onClick={handleClick}
-                className="text-xl text-gray-700 hover:text-orange-600 transition-colors py-2"
-              >
-                {item.label}
-              </a>
-            ))}
-          </nav>
-        </div>
+        <>
+          <div
+            className="fixed inset-0 bg-black/50 z-40 md:hidden"
+            onClick={() => setIsOpen(false)}
+          />
+          <div className="fixed top-16 left-0 right-0 bg-white shadow-lg z-50 md:hidden">
+            <nav className="flex flex-col p-6 space-y-4">
+              {navItems.map((item) => (
+                <a
+                  key={item.href}
+                  href={item.href}
+                  onClick={handleClick}
+                  className="text-lg text-gray-700 hover:text-red-600 transition-colors py-2 border-b border-gray-100 last:border-0"
+                >
+                  {item.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </>
       )}
     </>
   );
